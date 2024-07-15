421dda70d8d68033dd8ae6029e9cfb6be4eb90e6
Yi-Ting Chiu <mytim710@gmail.com>
2024-07-15 17:23:42 +0800

    Update llm_factory.py

--- llm/llm_factory.py
+++ llm/llm_factory.py
@@ -4,17 +4,6 @@ from llm.memGPT import LLM as MemGPTLLM
 from llm.fake_llm import LLM as FakeLLM
 from typing import Type
 
-import yaml
-import os
-# load configurations
-def load_config():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    config_path = os.path.join(dir_path, "memgpt_config.yaml")
-    with open(config_path, "r") as f:
-        return yaml.safe_load(f)
-config = load_config()
-def get_config(key, default=None):
-    return config.get(key, default)
 
 class LLMFactory:
     @staticmethod
