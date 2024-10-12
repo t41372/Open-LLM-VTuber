74e7c5d9d6f425ea6b41ecd40303d911f79a9798
Yi-Ting Chiu <mytim710@gmail.com>
2024-10-12 01:07:20 -0700

    feat: load var from env with ${ENV}

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import re
 import shutil
 import atexit
 import json
@@ -194,13 +195,45 @@ class WebSocketServer:
             os.makedirs(cache_dir)
 
 
+def load_config_with_env(path) -> dict:
+    """
+    Load the configuration file with environment variables.
+
+    Parameters:
+    - path (str): The path to the configuration file.
+
+    Returns:
+    - dict: The configuration dictionary.
+
+    Raises:
+    - FileNotFoundError if the configuration file is not found.
+    - yaml.YAMLError if the configuration file is not a valid YAML file.
+    """
+    with open(path, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    # Match ${VAR_NAME}
+    pattern = re.compile(r"\$\{(\w+)\}")
+
+    # replace ${VAR_NAME} with os.getenv('VAR_NAME')
+    def replacer(match):
+        env_var = match.group(1)
+        return os.getenv(
+            env_var, match.group(0)
+        )  # return the original string if the env var is not found
+
+    content = pattern.sub(replacer, content)
+
+    # Load the yaml file
+    return yaml.safe_load(content)
+
+
 if __name__ == "__main__":
 
     atexit.register(WebSocketServer.clean_cache)
 
     # Load configurations from yaml file
-    with open("conf.yaml", "rb") as f:
-        config = yaml.safe_load(f)
+    config = load_config_with_env("config.yaml")
 
     config["LIVE2D"] = True  # make sure the live2d is enabled
 
