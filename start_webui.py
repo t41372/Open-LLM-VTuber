26ca0ebd3d11d3fba9637ef0e0a0039390aa958c
SunKSugaR <sunksugar24@gmail.com>
2024-12-03 00:38:11 +0800

    bugfix: env won't reinstall if it doesn't exist.

--- start_webui.py
+++ start_webui.py
@@ -23,6 +23,7 @@ def main():
         install_mgr.setup()
 
     # Activate environment and run server
+    install_mgr.check_environment()
     if platform.system().lower() == "windows":
         activate_cmd = f"call {install_mgr.activate_script} {install_mgr.env_name}"
     else:
