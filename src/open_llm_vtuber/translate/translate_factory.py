904da5e5b42ac2df482d68ef9d0cce676251c524
Fluchw <2427607997@qq.com>
2025-01-30 19:10:26 +0800

    1.接入了腾讯翻译 2.解决了使用gptsovits作为语音模型时,文本和语音的语言不一致会出现的bug

--- src/open_llm_vtuber/translate/translate_factory.py
+++ src/open_llm_vtuber/translate/translate_factory.py
@@ -1,4 +1,5 @@
 from .deeplx import DeepLXTranslate
+from .tencent import TencentTranslate
 from .translate_interface import TranslateInterface
 
 
@@ -12,6 +13,14 @@ class TranslateFactory:
             return DeepLXTranslate(
                 api_endpoint=translate_provider_config.get("deeplx_api_endpoint"),
                 target_lang=translate_provider_config.get("deeplx_target_lang"),
+            )
+        elif translate_provider == "tencent":
+            return TencentTranslate(
+                secret_id=translate_provider_config.get("secret_id"),
+                secret_key=translate_provider_config.get("secret_key"),
+                region=translate_provider_config.get("region"),
+                source_lang=translate_provider_config.get("source_lang"),
+                target_lang=translate_provider_config.get("target_lang"),
             )
         else:
             raise ValueError(f"Unsupported translate provider: {translate_provider}")
